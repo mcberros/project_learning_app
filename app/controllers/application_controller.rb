6690834b28ee5867ed6866b2827fe7d181fda04d
mcberros <mcberros@gmail.com>
2013-07-24 00:24:36 +0200

    Access pages only with user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_filter :require_login
+
   protect_from_forgery
   helper_method :current_user
 
   private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to log_in_path
+    end
+  end
+
+  def logged_in?
+    !!current_user
   end
 end
--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,6 @@
 class PasswordResetsController < ApplicationController
+  skip_before_filter :require_login, :only => [:new, :create]
+  
   def new
   end
 
