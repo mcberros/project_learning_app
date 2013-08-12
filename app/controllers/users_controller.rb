bcd540c24616a61c68705e4fccc106105ad497fc
mcberros <mcberros@gmail.com>
2013-08-12 23:43:27 +0200

    Use of auth token and draper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,35 @@
 class UsersController < ApplicationController
-  skip_before_filter :require_login, :only => [:new, :create]
+  skip_before_filter :require_login
+  
+  decorates_assigned :user
   
   respond_to :html
+
+  def index
+    @users = User.all
+  end
 
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new()
+    @user.email=params[:user][:email]
+    @user.password=params[:user][:password]
     if @user.save
+      if params[:user][:remember_me]
+        cookies.permanent[:auth_token] =  @user.auth_token
+      else
+        cookies[:auth_token] =  @user.auth_token
+      end
       redirect_to root_url, :notice => 'Signed up!'
     else
       render 'new'
     end  
+  end
+  
+  def show
+    @user = User.find(params[:id]).decorate
   end
 end
