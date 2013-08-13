f266f962bfd442742ef6f66e0d08498522ab3341
mcberros <mcberros@gmail.com>
2013-08-13 16:42:38 +0200

    Using modern notation for hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   before_create { generate_token(:auth_token) }
 
   validates_confirmation_of :password
-  validates_presence_of :password, :on => :create
+  validates_presence_of :password, on: :create
   validates_presence_of :email
   validates_uniqueness_of :email
 
